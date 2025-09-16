f9151cb52500b1028b6fc7e4f253961abe87607f
nguyenmanhdung23020520 <23020520@vnu.edu.vn>
2025-09-16 08:45:45 +0700

    update

--- src/main/java/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package respritatory;
-import model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface respirtory extends JpaRepository<Course, Long>{
-
-}
